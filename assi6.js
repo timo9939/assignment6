1891167ffb1ebc49ae297d976c72adc43947b461
timo9939 <timothylau20199@gmail.com>
2022-11-06 18:09:51 +0000

    weather icon update

--- assi6.js
+++ assi6.js
@@ -8,10 +8,23 @@ let cityName1=document.getElementById('cityName1')
 let day1Temp=document.getElementById('day1-temp')
 let day1Wind=document.getElementById('day1-wind')
 let day1Humid=document.getElementById('day1-humid')
+let latitude=""
+let longitude=""
+let icon1=""
+let searchEle=document.getElementById('search-list')
+let searchEleText=""
+const clearStorage=document.getElementById("clearStorageBtn")
+
+function search(event){
+   event.preventDefault();
+
+   // Storing the city's name input
+   searchEleText=cityWebsite.textContent;
+   window.localStorage.setItem("inputStorage",searchEleText)
+   searchEleText=window.localStorage.getItem("inputStorage")
+   searchEle.append(searchEleText);
 
 
-function search(){
-   
     //const API="32fa6c827151a5c195199dc77fd3d23f";
 
     alert("work")
@@ -26,12 +39,18 @@ fetch(requestUrl)
     console.log("data is ",data);
     console.log("name is "+data.name);
    
-    
+    //Day 1 weather
     let cityName1El =document.createElement('h1');
     cityName1El.textContent=data.name;
     cityName1.append(cityName1El);
+    
+    
+    icon1=data.weather[0].icon
+    console.log("icon1: "+icon1)
+    document.getElementById('img1').src= ('http://openweathermap.org/img/wn/'+icon1+'.png')
+    
 
-    let day1TempEl=document.createElement('h3')
+    let day1TempEl=document.createElement('h4')
     day1TempEl.textContent=Math.round(data.main.temp-273.15);
     day1Temp.append(day1TempEl);
     console.log("Day1 Temperature: "+Math.round(data.main.temp-273.15));
@@ -45,12 +64,35 @@ fetch(requestUrl)
     humidityEl.textContent=(data.main.humidity+'%')
     day1Humid.append(humidityEl);
     
-    }
-   
-  
+    
 
-)}
+    latitude=data.coord.lat.textContent;
+    longitude=data.coord.lon.textContent;
+    
 
+    
+    /*
+    //Day2 weather
+    let requestUrl2 ="api.openweathermap.org/data/2.5/forecast?lat="+latitude+"&lon="+longitude+"&appid=32fa6c827151a5c195199dc77fd3d23f";
+    fetch(requestUrl2)
+    .then(function(response2){
+    console.log(response2)
+    return response2.json()
+    })
 
+    .then(function(data2){
+    console.log("data is ",data2);
+    console.log("name is "+data2.name);
+    })*/
+    })
+    
+
+}
+
+function clear(event){
+    event.preventDefault();
+    window.localStorage.clear()
+}
 
 searchButton.addEventListener("click",search);
+clearStorage.addEventListener("click",clear);
